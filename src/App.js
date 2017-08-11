6cffe9a014a983e5969948db538861d9b1c79db7
Charlie Lambropoulos <charlie101010@gmail.com>
2017-08-11 11:52:04 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: true
+    showSearchPage: false
   }
 
   render() {
@@ -21,7 +21,7 @@ class BooksApp extends React.Component {
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
               <div className="search-books-input-wrapper">
-                {/* 
+                {console.log(BooksAPI.getAll())/* 
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
---
+++ src/shelves.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+
+
+class Shelves extends React.Component {
+
+
+
+}
+
+
+export default Shelves
