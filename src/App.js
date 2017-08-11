946527067e5a69f0771d6ee64ae2504f6be81e05
Charlie Lambropoulos <charlie101010@gmail.com>
2017-08-11 13:32:28 -0700

    setup filter map on shelves

--- src/App.js
+++ src/App.js
@@ -7,23 +7,23 @@ import {Route} from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
     return (
       <div className="app">
       <Route path='/search' render={() =>
-         <Search />
+         <Search books={this.state.books}/>
         } />
         <Route exact path='/' render={() =>
-         <Shelves />
+         <Shelves books={this.state.books}/>
         } />
       </div>
     )
--- src/search.js
+++ src/search.js
@@ -1,13 +1,47 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import {Link} from 'react-router-dom'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+import PropTypes from 'prop-types'
 
 
 
 class Search extends React.Component {
+	static propTypes = {
+		books: PropTypes.array.isRequired,
+	}
+
+
+	state = {
+		query: ''
+	}
+
+	updateQuery = (e) => {
+		this.setState({
+			query: e.target.value
+		})
+	}
+
+
 	render(){
+		
+		let showingBooks
+		if(this.state.query){
+			const match = new RegExp(escapeRegExp(this.state.query), 'i')
+			showingBooks = this.props.books.filter((book) => match.test(book.title))
+
+		} else{
+			showingBooks = this.props.books
+		}
+		showingBooks.sort(sortBy('title'))
+
+
+
+
+
 		return(
-	 <div className="search-books">
+	 		<div className="search-books">
             <div className="search-books-bar">
               <Link className="close-search" to='/'>Close</Link>
               <div className="search-books-input-wrapper">
@@ -19,12 +53,36 @@ class Search extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+                <input type="text" value={this.state.query} onChange={this.updateQuery} placeholder="Search by title or author"/>
+
                 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+
+              <ol className="books-grid">
+              {showingBooks.map((book) => (
+          	     <li key={book.id}>
+                    <div className="book">
+                      <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                        <div className="book-shelf-changer">
+                          <select>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">{book.authors}</div>
+                    </div>
+                  </li>
+              	))}
+              </ol>
+
             </div>
           </div>
 
